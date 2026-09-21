1d20f09072aa60a0203d6266cf571a7b7f539606
Damodhar-7823 <damu7823@gmail.com>
2026-09-21 17:57:15 +0530

    food ordering system

---
+++ src/main/java/com/springboot/FoodOrderingSystem/security/CustomUserDetails.java
@@ -0,0 +1,36 @@
+package com.springboot.FoodOrderingSystem.security;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.springboot.FoodOrderingSystem.entity.User;
+
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        // SGA creates an array and stores the role of the user. Therefore, convert array to list and return it using Arrays.asList()
+        return Arrays.asList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
---
+++ src/main/java/com/springboot/FoodOrderingSystem/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.springboot.FoodOrderingSystem.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.springboot.FoodOrderingSystem.entity.User;
+import com.springboot.FoodOrderingSystem.repository.UserRepository;
+import com.springboot.FoodOrderingSystem.security.CustomUserDetails;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return new CustomUserDetails(user);
+    }
+}
